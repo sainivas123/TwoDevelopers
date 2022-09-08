a2c7217b13a87bdd3fddb07f0b0c21448ca1d320
sainivas123 <100339516+sainivas123@users.noreply.github.com>
2022-09-08 16:37:55 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,19 @@
+#This is Test Code 
 from code import hw
 import unittest
-print("This is Testing file")
+
+class test1(unittest):
+  def setup(self):
+    print("Self up")
+  
+  def tearDown(self):
+    print("Tear down")
+  
+  def test_main(self):
+    # Code for test
+    self.assertEqual(3,3,"We are equal")
+    
+if __name__=="__main__":
+  unittest.main()
+  
+
