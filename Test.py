60290c43d2f132a5012c0955991916d350a7d311
sainivas123 <100339516+sainivas123@users.noreply.github.com>
2022-09-08 16:45:30 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,25 +2,18 @@
 #from code import hw
 import unittest
 
-
 class SimpleCalll(unittest.TestCase):
 
      def setUp(self):
-          print("Setup")
-
-
+          print("Set Up")
+          
      def tearDown(self):
-          print("Tear down")
-
+          print("Tear Down")
 
      def test_main(self):
          #Code for the test
          self.assertEqual(3,3,'We are Equal')
-         #This will Fail
-         self.assertGreater(3,4,"3 is not greater than 4")
-
-
-
+         
 if __name__ == '__main__':
     unittest.main()
 
